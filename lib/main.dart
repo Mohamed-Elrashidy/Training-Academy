bf34c88502bdeef28134a2225eb8e4d90f12cc3e
MuhammedElrashidy <meahef@gmail.com>
2026-06-06 10:21:15 +0300

    authentication almost finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:training_acedamy/core/service/crashlytics/crashlytics_initializer.dart';
 import 'package:training_acedamy/training_academy_app.dart';
